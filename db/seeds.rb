f7c7ef5c5927c84decdf68afe79a082f9136951c
Watila Costa <watilafreire26@gmail.com>
2025-07-11 11:15:03 -0300

    criando usuário para acessar o painel do sistema. Esse usuário deve ser admin preferencialmente.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Criando usuáro admin"
+
+user = User.find_or_create_by!(email: 'admin@example.com') do |user|
+  user.password = "12345678"
+  user.contact_number = '5599999999999'
+  user.first_name = 'Admin'
+  user.last_name = 'User'
+  user.confirmed_at = Time.current
+end
+
+puts "Usuáio Admin criado com sucesso: "
+puts "email: #{user.email}"
